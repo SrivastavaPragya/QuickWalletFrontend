4e4a57bff715f32b0e73814be8823b3df8be3e93
Pragya <pragya142018@gmail.com>
2024-07-13 12:51:09 +0530

    adding components

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Home1 from '../components/Home/Home1'
 import Home2 from '../components/Home/Home2'
+import Home3 from '../components/Home/Home3'
+import Home4 from '../components/Home/Home4'
+import Home5 from '../components/Home/Home5'
 
 const HomePage = () => {
   return (
     <div>
       <Home1/>
       <Home2/>
+      <Home3/>
+      <Home4/>
+      <Home5/>
     </div>
   )
 }
---
+++ src/components/Home/Home3.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home3 = () => {
+  return (
+    <div>
+        <div className="Home2Container">
+        <h3>OUR SERVICES</h3>
+        <h1>Smart Solution for Your Payment</h1>
+        <img src="./services.png" alt="" />
+        </div>
+    
+    </div>
+  )
+}
+
+export default Home3
