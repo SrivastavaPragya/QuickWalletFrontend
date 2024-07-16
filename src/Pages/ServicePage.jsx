860e2855e264c10f62fa8c1f6ef9548f03d3572a
Pragya <pragya142018@gmail.com>
2024-07-16 12:57:48 +0530

    adding components

--- src/Pages/ServicePage.jsx
+++ src/Pages/ServicePage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Service1 from '../components/Service/Service'
 import Service2 from '../components/Service/Service2'
+import Service3 from '../components/Service/Service3'
+import Service4 from '../components/Service/Service4'
 const ServicePage = () => {
   return (
     <div>
 <Service1/>
 <Service2/>
+<Service3/>
+<Service4/>
     </div>
   )
 }
--- src/components/Service/Service3.jsx
+++ src/components/Service/Service3.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Service3 = () => {
   return (
     <div>
-      this is service 3jsx
+     <div className="service3Container">
+        <div className="service3Content">
+        <h3>PAYMENT GATEWAY</h3>
+        <h1>The Best Solution for Accepting Online Payments</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaQuis ipsum suspendisse ultrices gravida.</p>
+        </div>
+       
+        <img src="./image-videosession.png" alt="" />
+     </div>
     </div>
   )
 }
